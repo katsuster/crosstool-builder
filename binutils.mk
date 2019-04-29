4870a2fb76624efa66e5a3312e4697305257fb2b
Katsuhiro Suzuki <katsuhiro@katsuster.net>
2019-04-30 00:32:55 +0900

    makefile: first version of binutils

---
+++ binutils.mk
@@ -0,0 +1,38 @@
+
+SRC_NAME       ?= binutils
+BUILD_NAME     ?= $(SRC_NAME)
+BUILDER_NAME   ?= $(BUILD_NAME).mk
+
+CONFIGURE_NAME ?= $(SRC_DIR)/configure
+MAKEFILE_NAME  ?= $(BUILD_DIR)/Makefile
+BINARY_NAME    ?= FORCE
+
+include common.mk
+
+# Define body targets.
+
+configure-body:
+	cd $(BUILD_DIR) && \
+	$(SRC_DIR)/configure \
+	  --target=$(CROSS_ARCH) \
+	  --prefix=$(CROSS_ROOT) \
+	  --disable-nls \
+	  --disable-static \
+	  --disable-werror \
+	  --with-lib-path=$(CROSS_ROOT)/lib \
+	  --with-sysroot=$(CROSS_ROOT)
+
+build-body:
+	$(MAKE) -f $(BUILDER_NAME) $@-default
+
+install-body:
+	$(MAKE) -f $(BUILDER_NAME) $@-default
+
+clean-body:
+	$(MAKE) -f $(BUILDER_NAME) $@-default
+
+distclean-body:
+	$(MAKE) -f $(BUILDER_NAME) $@-default
+
+allclean-body:
+	$(MAKE) -f $(BUILDER_NAME) $@-default
