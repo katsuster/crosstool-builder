44e8cfdbc8822f59b5771b24d96a6fab42addc4a
Katsuhiro Suzuki <katsuhiro@katsuster.net>
2019-04-30 00:36:02 +0900

    env: add arch environments

--- env.sh
+++ env.sh
@@ -1,11 +1,25 @@
 #!/bin/sh
 
-export CROSS_ARCH=arm-linux-gnueabi
-export CROSS_ROOT=`pwd`/buildroot
+# For AArch32
+#export CROSS_ARCH=arm-unknown-linux-gnueabi
+#export LINUX_ARCH=arm
+
+# For AArch64
+#export CROSS_ARCH=aarch64-unknown-linux-gnu
+#export LINUX_ARCH=arm64
+
+# For RISC-V 64
+export CROSS_ARCH=riscv64-unknown-linux-gnu
+export LINUX_ARCH=riscv
+
+export TOP_DIR=`pwd`
+export CROSS_ROOT=$TOP_DIR/buildroot
 if [ x`echo $PATH | grep $CROSS_ROOT/bin` = x ]; then
 	export PATH=$PATH:$CROSS_ROOT/bin
 fi
 
 echo "CROSS_ARCH: '$CROSS_ARCH'"
+echo "LINUX_ARCH: '$LINUX_ARCH'"
+echo "TOP_DIR   : '$TOP_DIR'"
 echo "CROSS_ROOT: '$CROSS_ROOT'"
 echo "PATH      : '$PATH'"
