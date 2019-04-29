7469e6296b6e4f10e275fa4b2cc1a62f873c15ff
Katsuhiro Suzuki <katsuhiro@katsuster.net>
2019-04-30 00:34:16 +0900

    makefile: first version of gcc

---
+++ gcc-shared.mk
@@ -0,0 +1,53 @@
+
+SRC_NAME       ?= gcc
+BUILD_NAME     ?= $(SRC_NAME)-shared
+BUILDER_NAME   ?= $(BUILD_NAME).mk
+
+CONFIGURE_NAME ?= $(SRC_DIR)/configure
+MAKEFILE_NAME  ?= $(BUILD_DIR)/Makefile
+BINARY_NAME    ?= FORCE
+
+include common.mk
+
+# Define body targets.
+
+configure-body:
+	cd $(BUILD_DIR) && \
+	$(SRC_DIR)/configure \
+	  --target=$(CROSS_ARCH) \
+	  --prefix=$(PREFIX) \
+	  --enable-languages=c,c++ \
+	  --disable-libstdcxx-pch \
+	  --enable-libgomp \
+	  --enable-libmudflap \
+	  --enable-libquadmath \
+	  --enable-libssp \
+	  --enable-libatomic \
+	  --disable-libitm \
+	  --disable-libsanitizer \
+	  --disable-multiarch \
+	  --disable-multilib \
+	  --enable-nls \
+	  --enable-plugin \
+	  --enable-shared \
+	  --enable-threads=posix \
+	  --enable-__cxa_atexit \
+	  --with-local-prefix=$(SYSROOT)/usr \
+	  --with-build-sysroot=$(SYSROOT) \
+	  --with-sysroot=$(SYSROOT) \
+	  --with-native-system-header-dir=/usr/include
+
+build-body:
+	$(MAKE) -f $(BUILDER_NAME) $@-default
+
+install-body:
+	$(MAKE) -f $(BUILDER_NAME) $@-default
+
+clean-body:
+	$(MAKE) -f $(BUILDER_NAME) $@-default
+
+distclean-body:
+	$(MAKE) -f $(BUILDER_NAME) $@-default
+
+allclean-body:
+	$(MAKE) -f $(BUILDER_NAME) $@-default
