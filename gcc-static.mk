fa9607279b6efcca0e0479da55415d252b2a65cf
Katsuhiro Suzuki <katsuhiro@katsuster.net>
2022-08-04 14:43:55 +0900

    mk: gcc-static: add arch dependent cflags

--- gcc-static.mk
+++ gcc-static.mk
@@ -14,10 +14,10 @@ include common.mk
 configure-body:
 	cd $(BUILD_DIR) && \
 	$(SRC_DIR)/configure \
-	  CFLAGS="-g -O0 -fno-inline" \
-	  CXXFLAGS="-g -O0 -fno-inline" \
-	  CFLAGS_FOR_TARGET="-g -O0 -fno-inline" \
-	  CXXFLAGS_FOR_TARGET="-g -O0 -fno-inline" \
+	  CFLAGS="-g -O0 -fno-inline $(ARCH_CFLAGS)" \
+	  CXXFLAGS="-g -O0 -fno-inline $(ARCH_CFLAGS)" \
+	  CFLAGS_FOR_TARGET="-g -O0 -fno-inline $(ARCH_CFLAGS_FOR_TARGET)" \
+	  CXXFLAGS_FOR_TARGET="-g -O0 -fno-inline $(ARCH_CXXFLAGS_FOR_TARGET)" \
 	  --target=$(CROSS_ARCH) \
 	  --prefix=$(PREFIX) \
 	  --enable-languages=c \
